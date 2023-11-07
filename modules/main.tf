133937789b01e79ef249363fb4f0950b7e42afdd
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-07 23:49:36 +0530

    Create main.tf

---
+++ modules/main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = var.region
+}
+
+module "network" {
+  source                           = "./modules/network"
+  create_vpc                       = var.create_vpc
+   cidr                             = var.cidr
+ _default_security_group    = var.manage_default_security_group
+  default_security_group_ingress   = var.default_security_group_ingress
+  default_security_group_egress    = var.default_security_group_egress
+  }
