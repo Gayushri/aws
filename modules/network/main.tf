3e57d4258cfff03580c1b07d3ca07a59ac0abf78
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-07 23:06:11 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -1,9 +1,28 @@
 resource "aws_vpc" "vpc" {
-   cidr_block       = "10.0.0.0/24"
-  instance_tenancy = "default"
+   cidr_block       = "var.use_ipam"
+ }
 
-  tags = {
-    Name = "web"
+resource "aws_default_security_group" "security_group" {
+  vpc_id = aws_vpc.vpc.id
+
+  dynamic "ingress" {
+    for_each = var.default_security_group_ingress
+    content {
+      self             = lookup(ingress.value, "self", null)
+      from_port        = lookup(ingress.value, "from_port", 0)
+      to_port          = lookup(ingress.value, "to_port", 0)
+      protocol         = lookup(ingress.value, "protocol", "-1")
+    }
   }
-}
+
+  dynamic "egress" {
+    for_each = var.default_security_group_egress
+    content {
+      self             = lookup(egress.value, "self", null)
+      from_port        = lookup(egress.value, "from_port", 0)
+      to_port          = lookup(egress.value, "to_port", 0)
+      protocol         = lookup(egress.value, "protocol", "-1")
+    }
+  }
+
 
