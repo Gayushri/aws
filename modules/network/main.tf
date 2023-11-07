624237a03870a256e30f4d995c62379f9ce35036
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-08 00:36:23 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -27,4 +27,17 @@ vpc_id = aws_vpc.vpc.id
     }
   }
 
+resource "aws_s3_bucket" "demo_bucket" {
+  for_each = toset(var.bucket_list)
+bucket = each.key
+key = var.key
+force_destroy = var.force_destroy
+acl = var.acl
+storage_class = var.storage_class
+source = var.file_source
+content = var.content
+content_base64 = var.content_base64
+tag = var.tag
+region = var.region
+}
 
