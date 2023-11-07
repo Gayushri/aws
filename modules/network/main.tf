fd0e3cc89870fd04cde723477f280b95c1d3d24f
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-08 01:37:28 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -26,6 +26,33 @@ vpc_id = aws_vpc.vpc.id
       protocol         = tcp
     }
   }
+resource "aws_subnet" "public" {
+count      = length(var.private_subnet_cidrs)
+vpc_id       = var.vpc_id
+cidr_block  = element(var.public_subnets_cidrs, count.index)
+availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "public subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_subnet" "private" {
+count      = length(var.private_subnet_cidrs)
+vpc_id       = var.vpc_id
+cidr_block  = element(var.private_subnets_cidrs, count.index)
+ availability_zone = element(var.azs, count.index)
+ tags = {
+   Name = "private subnet ${count.index + 1}"
+ }
+}
+
+resource "aws_internet_gateway" "gw" {
+ vpc_id = aws_vpc.id
+ 
+ tags = {
+   Name = "Project VPC IG"
+ }
+}
 
 resource "aws_s3_bucket" "demo_bucket" {
   for_each = toset(var.bucket_list)
