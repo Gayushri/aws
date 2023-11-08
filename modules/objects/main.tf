47da0c1a710b92f9e68d381ccd3fb35902bb0c2b
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-08 14:09:21 +0530

    Update main.tf

--- modules/objects/main.tf
+++ modules/objects/main.tf
@@ -1,18 +1,14 @@
 resource "aws_s3_object" "demo-buc" {
-  count = var.create ? 1 : 0
-
+ count = 2
   bucket        = var.bucket
   key           = var.key
   force_destroy = var.force_destroy
-
   acl           = var.acl
   storage_class = try(upper(var.storage_class), var.storage_class)
-
   source         = var.file_source
   content        = var.content
   content_base64 = var.content_base64
   etag           = var.etag
-
   cache_control       = var.cache_control
   content_disposition = var.content_disposition
   content_encoding    = var.content_encoding
@@ -20,13 +16,8 @@ resource "aws_s3_object" "demo-buc" {
   content_type        = var.content_type
   website_redirect    = var.website_redirect
   metadata            = var.metadata
-
   server_side_encryption = var.server_side_encryption
   kms_key_id             = var.kms_key_id
   bucket_key_enabled     = var.bucket_key_enabled
-
-
   tags = var.tags
-
- 
 }
