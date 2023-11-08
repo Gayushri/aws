760afbe74032f3ec992bf5e7693b49d186df39f2
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-08 11:44:03 +0530

    Update main.tf

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -60,17 +60,7 @@ resource "aws_internet_gateway" "gw" {
  }
 }
 
-resource "aws_s3_bucket" "demo_bucket" {
-  for_each = toset(var.bucket_list)
-bucket = each.key
-key = var.key
-force_destroy = var.force_destroy
-acl = var.acl
-storage_class = var.storage_class
-source = var.file_source
-content = var.content
-content_base64 = var.content_base64
-tag = var.tag
+
 region = var.region
 }
 
