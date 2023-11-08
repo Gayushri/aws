1e00c2ccff6d4faff915a2f9fe5a4b52da5eb091
Gayushri <121676999+Gayushri@users.noreply.github.com>
2023-11-08 11:44:35 +0530

    Update main.tf

--- modules/objects/main.tf
+++ modules/objects/main.tf
@@ -1,4 +1,11 @@
 resource "aws_s3_bucket" "demo_bucket" {
   for_each = toset(var.bucket_list)
-  bucket = each.key
-}
+bucket = each.key
+key = var.key
+force_destroy = var.force_destroy
+acl = var.acl
+storage_class = var.storage_class
+source = var.file_source
+content = var.content
+content_base64 = var.content_base64
+tag = var.tag
